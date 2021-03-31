3dae669f5e552231e4dffdd5201a8577128de322
ARHALE <halea3@mail.uc.edu>
2021-03-31 02:22:18 +0000

    fixed several errors

--- Counter.java
+++ Counter.java
@@ -1,60 +1,62 @@
-import java.awt*;
-import java.awt.event*;
-import javax.swing*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 //create Counter class
-public class Counter implements  ActionListener {
-  
-  //declare  instance variables
-  Jlabel leftLabel;
-  Jlabel rightLabel;
+public class Counter implements ActionListener {
+
+  //declare  instance variables  
+  JLabel leftLabel;
+  JLabel rightLabel;
   int leftCount;
   int rightCount;
 
   //Counter  Constructor Creation
-  Counter(){
-    //set frame
-    JFrame frame = new JFrame("Let's Count");
-    frame.setLayout(new FlowLayout());
-    
+ Counter() {
+   //set frame
+   JFrame frame = new JFrame("Let's Count");
+   frame.setLayout(new FlowLayout());
+   
     //set size of frame
-    frame.setSize(200,  110);
+   frame.setSize(200,110);
 
     //create buttons
-    JButton leftButton = new JButton("Left");
-    JButton rightButton = new JButton("Right");
+   JButton leftButton = new JButton("Left");
+   JButton rightButton = new JButton("Right");
 
-    int leftCount = 0;
-    int rightCount = 0;
+   leftButton.addActionListener(this);
+   rightButton.addActionListener(this);
 
-    JLabel leftLabel = new JLabel("Count: " + leftCount);
-    JLabel rightLabel = new JLabel("Count: " + rightCount);
+   int leftCount = 0;
+   int rightCount = 0;
 
-    //Create action listeners
-    leftButton.addActionListener(this);
-    rightButton.addActionListener(this);
-    //create reset button
-    JButton reset = new JButton("Reset");
-    reset.addActionListener(this);
 
-   //add labes and buttons to frame
    frame.add(leftButton);
    frame.add(rightButton);
+
+   leftLabel = new JLabel ("Count: " +leftCount);
+   rightLabel = new JLabel ("Count: "+rightCount);
+   
    frame.add(leftLabel);
    frame.add(rightLabel);
+
+   JButton reset = new JButton("Reset");
+   reset.addActionListener(this);
    frame.add(reset);
-   //setframe  visible
+
    frame.setVisible(true);
-  }
-  //create action  performed method
+   
+   
+    }
+
     public void actionPerformed(ActionEvent ae) {
       if(ae.getActionCommand().equals("Left")){
-        leftCount = leftCount + 1;
+        leftCount++;
         leftLabel.setText("Count: " + leftCount);
       }
 
       else if(ae.getActionCommand().equals("Right")){
-        rightCount = rightCount + 1;
+        rightCount++;
         rightLabel.setText("Count: " + rightCount);
       }
 
@@ -65,4 +67,4 @@ public class Counter implements  ActionListener {
         rightLabel.setText("Count: " +rightCount);
       }
     }
-}
+   }
